e5f99b2e063ce7075beea7ac75fc2545d9eaa0f8
TP <thanhpn1@yahoo.com>
2022-10-04 16:27:14 -0500

    add functionalities to design patterns section

--- beep-n-bop.js
+++ beep-n-bop.js
@@ -1,22 +1,73 @@
 const patternsContainer = document.querySelector('.patterns-container');
 const panels = document.querySelectorAll('.panel');
 const patternsList = document.querySelector('.patterns-list');
-const pattern = document.querySelectorAll('.pattern-item');
+const patternItems = document.querySelectorAll('.pattern-item');
+const smallPrints = document.querySelectorAll('small');
 
 patternsContainer.addEventListener('click', (e) => {
   //   console.log(e.target);
   if (e.target.classList.contains('panel')) {
-    // remove active class from all panels
-    panels.forEach((panel) => {
-      panel.classList.remove('active');
+    const caption = e.target.dataset.caption;
+    removeActiveClass();
+    hideSmallPrints();
+    // find the elements with the same data-caption:
+    const elements = document.querySelectorAll(`[data-caption=${caption}]`);
+    // add active class to the elements
+    elements.forEach((element) => {
+      element.classList.add('active');
+      //   find the small element and display it
+      if (element.firstElementChild) {
+        const smallElement = element.firstElementChild;
+        smallElement.classList.remove('hidden');
+      }
     });
-    // add active class to clicked panel
-    e.target.classList.add('active');
   }
 });
 
 patternsList.addEventListener('mouseover', (e) => {
   if (e.target.classList.contains('pattern-item')) {
-    console.log(e.target);
+    const clicked = e.target;
+    const caption = clicked.dataset.caption;
+    removeActiveClass();
+    hideSmallPrints();
+    // add the hidden class back to the selected item
+    clicked.firstElementChild.classList.remove('hidden');
+    // find the elements with the same data-caption:
+    const elements = document.querySelectorAll(`[data-caption=${caption}]`);
+    // add active class to the elements
+    elements.forEach((element) => {
+      element.classList.add('active');
+    });
   }
 });
+
+// Helper function to remove active class from cards and list items
+function removeActiveClass() {
+  // remove active class from all panel cards
+  panels.forEach((panel) => {
+    panel.classList.remove('active');
+  });
+  // remove active class from all pattern items
+  patternItems.forEach((item) => {
+    item.classList.remove('active');
+  });
+}
+
+// Helper function to add hidden class to hide small prints
+function hideSmallPrints() {
+  smallPrints.forEach((item) => {
+    item.classList.add('hidden');
+  });
+}
+
+// // ✅ Get all elements with `data-id` attribute
+// const elements1 = document.querySelectorAll('[data-id]');
+// console.log(elements1); // 👉️ [div, div]
+
+// // ✅ Get only DIV elements with `data-id` attribute
+// const elements2 = document.querySelectorAll('div[data-id]');
+// console.log(elements2); // 👉️ [div, div]
+
+// // ✅ Get only elements where data-id = box1
+// const elements3 = document.querySelectorAll(`[data-id="box1"]`);
+// console.log(elements3); // 👉️ [div]
